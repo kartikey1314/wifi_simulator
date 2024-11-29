3b4d1f94239223905813637803f22dcc14928b4b
kartikey1314 <kartikeysinghrajput3014@gmail.com>
2024-11-29 09:33:47 +0530

    added wifi5simulator class and all the functionalities to it

--- wifi5simulator.cpp
+++ wifi5simulator.cpp
@@ -36,4 +36,71 @@ public:
         return p;
     }
 };
+class WiFi_5_Simulator{
+private:
+    vector<user> users;
+    double totalDataTransmitted;
+    double totalSimulationTime;
+    vector<double> latencies;
+public:
+    WiFi_5_Simulator(int numUsers, int numPacketsPerUser)
+        : totalDataTransmitted(0), totalSimulationTime(0){
+        if(numUsers<=0 || numPacketsPerUser<=0){ 
+            throw invalid_argument("Number of users and packets must be greater than zero.");
+        }
+        for(int i = 0; i < numUsers; ++i){
+            user user(i);
+            user.enqueuePacket(packet(CSI_SIZE));
+            for (int j=0;j<numPacketsPerUser;j++) {
+                user.enqueuePacket(packet(Pkt_size));
+            }
+            users.push_back(user);
+        }
+    }
+    void simulate(double durationMs){
+        double currentTime = 0;
+        while (currentTime < durationMs){
+            cout << "[INFO] Broadcast packet sent at " << currentTime << " ms.\n";
+            currentTime += CSI_TRANSMISSION_TIME;
+            for(auto& user : users){
+                if (!user.hasCSI && user.hasPackets()){
+                    packet csiPacket = user.dequeuePacket();
+                    currentTime += CSI_TRANSMISSION_TIME;
+                    user.hasCSI = true;
+                    cout << "[INFO] user " << user.id << " sent CSI at " << currentTime << " ms.\n";
+                }
+            }
+            cout << "[INFO] Parallel transmission starts at " << currentTime << " ms.\n";
+            for(int i = 0; i < PARALLEL_TRANSMISSION_TIME / transmission_time; i++){
+                for(auto& user : users){
+                    if(user.hasCSI && user.hasPackets()){
+                        packet dataPacket = user.dequeuePacket();
+                        totalDataTransmitted += dataPacket.size;
+                        double packetLatency = currentTime + transmission_time * i;
+                        dataPacket.latency = packetLatency;
+                        latencies.push_back(packetLatency);
+                        cout << "[DEBUG] user " << user.id << " transmitted a packet at " << packetLatency << " ms.\n";
+                    }
+                }
+            }
+            currentTime += PARALLEL_TRANSMISSION_TIME;
+            for(auto& user : users){
+                user.hasCSI = false;
+            }
+        }
+        totalSimulationTime = currentTime;
+        cout << "[INFO] Simulation Complete. Total Time = " << totalSimulationTime << " ms\n";
+    }
+    double throughput() const{
+        return (totalDataTransmitted * 8) / (totalSimulationTime * 1e3);
+    }
 
+    double averagelatency() const{
+        double sum = accumulate(latencies.begin(), latencies.end(), 0.0);
+        return latencies.empty() ? 0 : sum / latencies.size();
+    }
+
+    double maximumlatency() const{
+        return latencies.empty() ? 0 : *max_element(latencies.begin(), latencies.end());
+    }
+};
